0fa15ba0a0d88b2f6c9503d8dc1545ba449078ab
Michael Solati <mkslt04@gmail.com>
2017-10-01 18:45:21 -0400

    refactor(functions): migrate from geofire to geokit

--- functions/ct.js
+++ functions/ct.js
@@ -1,12 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const GeoFire = require('geofire');
+const Geokit = require('geokit').Geokit;
 const httpRequire = require('request');
+const geokit = new Geokit();
 admin.initializeApp(functions.config().firebase);
 
 exports.ct = functions.https.onRequest((request, response) => {
   const places = admin.database().ref('/places');
-  const geoFire = new GeoFire(admin.database().ref('/geofire/places'));
   const options = {
     method: 'GET',
     url: 'https://data.ct.gov/api/geospatial/sg4r-4tnv',
@@ -16,23 +16,19 @@ exports.ct = functions.https.onRequest((request, response) => {
     if (err) throw new Error(err);
     const locations = [];
     for (const feature of JSON.parse(body).features) {
+      const coordinates = {
+        lat: feature.geometry.coordinates[1],
+        lng: feature.geometry.coordinates[0]
+      };
       const location = {
         id: feature.properties.access_id,
         name: feature.properties.property,
         activities: [...new Set([feature.properties.prop_type, feature.properties.acc_type])],
-        coordinates: {
-          lat: feature.geometry.coordinates[1],
-          lng: feature.geometry.coordinates[0]
-        }
+        coordinates: coordinates,
+        hash: geokit.hash(coordinates)
       }
       locations.push(location);
       places.child(location.id).set(location);
-      const coords = [location.coordinates.lat, location.coordinates.lng];
-      geoFire.set(location.id, coords).then(() => {
-        console.log('Update succesfull');
-      }).catch(error => {
-        console.log(error);
-      });
     }
     response.send(JSON.stringify({ results: locations }));
   });
