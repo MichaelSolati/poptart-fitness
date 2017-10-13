626b631f052eaffc1c406bfcb1e26c2e07fca6e3
Robert Crowdis <RobertCrowdis@gmail.com>
2017-10-13 12:34:53 -0400

    feat(activities): instantiate activity service and firebase

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,18 +1,21 @@
+import { ActivitiesService } from './activities.service';
+export { ActivitiesService } from './activities.service';
+import { BadgesService } from './badges.service';
+export { BadgesService } from './badges.service';
 import { LocationService } from './location.service';
 export { LocationService } from './location.service';
 import { PlacesService } from './places.service';
 export { PlacesService } from './places.service';
-import { UserService } from './user.service';
-export { UserService } from './user.service';
-import { BadgesService } from './badges.service';
-export { BadgesService } from './badges.service';
 import { ProfilesService } from './profiles.service';
 export { ProfilesService } from './profiles.service';
+import { UserService } from './user.service';
+export { UserService } from './user.service';
 
 export const SERVICES: any[] = [
+  ActivitiesService,
+  BadgesService,
   LocationService,
   PlacesService,
+  ProfilesService,
   UserService,
-  BadgesService,
-  ProfilesService
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/first';
 
-import { ProfilesService } from '../core/services';
+import { ProfilesService, UserService } from '../core/services';
+import { ActivitiesService, IActivity } from '../core/services/activities.service';
 import { BadgesService, IBadge } from '../core/services/badges.service';
 
 @Component({
@@ -15,8 +18,12 @@ import { BadgesService, IBadge } from '../core/services/badges.service';
 export class ProfileComponent implements OnInit {
 
   private _idSubscription: Subscription;
+  private _user: Observable<any>;
+  private _badges: Observable<IBadge[]>;
+  private _activities: Observable<IActivity[]>;
 
-  private _activities: any[] = [
+
+  private _testactivities: any[] = [
     { name: 'Walking', liked: false },
     { name: 'Jogging', liked: false },
     { name: 'Running', liked: false },
@@ -28,9 +35,6 @@ export class ProfileComponent implements OnInit {
     { name: 'Camping-Underwater', liked: false },
   ];
 
-  private _user: Observable<any>;
-  private _badges: Observable<IBadge[]>;
-
   get badges() {
     return this._badges;
   }
@@ -39,13 +43,32 @@ export class ProfileComponent implements OnInit {
     return this._user;
   }
 
-  constructor(private _ps: ProfilesService, private _bs: BadgesService, private _route: ActivatedRoute) { }
+  get activities() {
+    return this._activities;
+  }
+
+  public goBack(): void {
+    this._location.back();
+  }
+
+  constructor(private _location: Location, private _us: UserService,
+              private _ps: ProfilesService, private _bs: BadgesService,
+              private _as: ActivitiesService, private _route: ActivatedRoute) { }
 
   ngOnInit() {
     this._idSubscription = this._route.params.subscribe(params => {
-      const id = params['id'] || 'aHeEWULkZYN7fQXl9ZHBprTNuMD2';
-      this._user = this._ps.findProfile(id);
-      this._badges = this._bs.findBadges(id);
+      const id = params['id'];
+      if (id) {
+        this._user = this._ps.findProfile(id);
+        this._badges = this._bs.findBadges(id);
+        this._activities = this._as.findActivities(id);
+      } else {
+        this._us.user.first().subscribe(user => {
+          this._user = this._ps.findProfile(user.uid);
+          this._badges = this._bs.findBadges(user.uid);
+          this._activities = this._as.findActivities(user.uid);
+        });
+      }
     });
   }
 
