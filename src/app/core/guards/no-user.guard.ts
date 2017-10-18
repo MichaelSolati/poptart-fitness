5d4eb9c98742ae4dbae8692c5e688532af0076fc
Michael Solati <mkslt04@gmail.com>
2017-10-17 20:31:15 -0400

    chore(docs): create docs for route guards

--- src/app/core/guards/no-user.guard.ts
+++ src/app/core/guards/no-user.guard.ts
@@ -7,10 +7,23 @@ import 'rxjs/add/operator/catch';
 
 import { UserService } from '../services';
 
+/**
+ * A class for the NoUserGuard
+ */
 @Injectable()
 export class NoUserGuard implements CanActivate {
+  /**
+   * @param _router Provides the navigation and url manipulation capabiliti.es.
+   * @param _us UserService that provides information on current user
+   */
   constructor(private _router: Router, private _us: UserService) { }
 
+  /**
+   * Determine if user can visit route. If signed in, redirect them.
+   * @param next Contains the information about a route associated with a component loaded in an outlet at a particular moment in time.
+   * @param state Represents the state of the router at a moment in time.
+   * @returns Boolean if user can navigate to a route. Will redirect if user may not visit.
+   */
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -28,6 +41,12 @@ export class NoUserGuard implements CanActivate {
       });
   }
 
+  /**
+   * Determine if user can visit child route by passing to canActivate function.
+   * @param next Contains the information about a route associated with a component loaded in an outlet at a particular moment in time.
+   * @param state Represents the state of the router at a moment in time.
+   * @returns Boolean if user can navigate to a route. Will redirect if user may not visit.
+   */
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
