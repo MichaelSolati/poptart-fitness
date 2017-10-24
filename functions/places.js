639648a4069ef5cc764b723bce5be89fba8c60ec
Michael Solati <mkslt04@gmail.com>
2017-10-24 17:57:22 -0400

    fix(wikipedia): set wikipedia as oncreate hook

--- functions/places.js
+++ functions/places.js
@@ -7,58 +7,53 @@ const geokit = new Geokit();
 
 exports.ct = functions.https.onRequest((request, response) => {
   const places = admin.database().ref('/places');
-  const options = {
-    method: 'GET',
-    url: 'https://data.ct.gov/api/geospatial/sg4r-4tnv',
-    qs: { method: 'export', format: 'GeoJSON' }
-  };
-  httpRequire(options, (err, res, body) => {
-    if (err) throw new Error(err);
-    const locations = [];
-    for (const feature of JSON.parse(body).features) {
-      const coordinates = {
-        lat: feature.geometry.coordinates[1],
-        lng: feature.geometry.coordinates[0]
-      };
-      const location = {
-        id: feature.properties.access_id,
-        name: feature.properties.property,
-        activities: [...new Set([feature.properties.prop_type, feature.properties.acc_type])],
-        coordinates: coordinates,
-        hash: geokit.hash(coordinates)
+  places.remove().then(() => {
+    httpRequire({
+      method: 'GET',
+      url: 'https://data.ct.gov/api/geospatial/sg4r-4tnv',
+      qs: { method: 'export', format: 'GeoJSON' }
+    }, (err, res, body) => {
+      if (err) throw new Error(err);
+      const locations = [];
+      for (const feature of JSON.parse(body).features) {
+        const coordinates = {
+          lat: feature.geometry.coordinates[1],
+          lng: feature.geometry.coordinates[0]
+        };
+        const location = {
+          id: feature.properties.access_id,
+          name: feature.properties.property,
+          activities: [...new Set([feature.properties.prop_type, feature.properties.acc_type])],
+          coordinates: coordinates,
+          hash: geokit.hash(coordinates)
+        }
+        locations.push(location);
+        places.child(location.id).set(location);
       }
-      locations.push(location);
-      places.child(location.id).set(location);
-    }
-    response.send(JSON.stringify({ results: locations }));
+      response.send(JSON.stringify({ results: locations }));
+    });
   });
 });
 
-exports.wikipedia = functions.https.onRequest((request, response) => {
-  const body = JSON.parse(request.body);
+exports.wikipedia = functions.database.ref('/places/{placeId}').onCreate((event) => {
   const places = admin.database().ref('/places');
-  if (!body.id) { throw new Error('No place id found'); }
-  places.child(body.id).on('value', (snapshot) => {
-    const place = snapshot.val();
-    if (place.description) { return response.send(JSON.stringify({ results: place.description })); }
-    const options = {
-      method: 'GET',
-      url: 'https://en.wikipedia.org/w/api.php',
-      qs: { action: 'query', prop: 'extracts', format: 'json', titles: place.name }
-    }
-    httpRequire(options, (err, res, wikiBody) => {
-      if (err) throw new Error(err);
-      let description;
-      try {
-        let wikiResult = JSON.parse(wikiBody).query.pages;
-        for (var prop in wikiResult) {
-          try {
-            description = wikiResult[prop].extract.replace(/<\/?[^>]+(>|$)/g, '');
-          } catch (e) { }
-          break;
-        }
-      } catch (e) { }
-      places.child(place.id).update({ description: description });
-    });
+  const place = event.data.val();
+  httpRequire({
+    method: 'GET',
+    url: 'https://en.wikipedia.org/w/api.php',
+    qs: { action: 'query', prop: 'extracts', format: 'json', titles: place.name }
+  }, (err, res, body) => {
+    if (err) throw new Error(err);
+    let description;
+    try {
+      let wikiResult = JSON.parse(body).query.pages;
+      for (var prop in wikiResult) {
+        try {
+          description = wikiResult[prop].extract.replace(/<\/?[^>]+(>|$)/g, '');
+        } catch (e) { }
+        break;
+      }
+    } catch (e) { }
+    return places.child(place.id).update({ description: description });
   });
 });
