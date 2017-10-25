3a71fb00b3d6be6cc621b906052e219b75e7dca5
Michael Solati <mkslt04@gmail.com>
2017-10-24 20:01:27 -0400

    refactor(profile): reorganize component to use data from profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,11 +6,9 @@ import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/first';
 
 import { ProfilesService, UserService } from '../core/services';
-import { ActivitiesService, IActivity } from '../core/services/activities.service';
-import { BadgesService, IBadge } from '../core/services/badges.service';
 
 /**
- * A component for the Profile
+ * A component for the ProfileComponent
  */
 @Component({
   moduleId: module.id,
@@ -18,53 +16,23 @@ import { BadgesService, IBadge } from '../core/services/badges.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-
+export class ProfileComponent implements OnInit, OnDestroy {
   private _idSubscription: Subscription;
   private _user: Observable<any>;
-  private _badges: Observable<IBadge[]>;
-  private _activities: Observable<IActivity[]>;
-
-  private _testactivities: any[] = [
-    { name: 'Walking', amount: 1 },
-    { name: 'Jogging', amount: 1 },
-    { name: 'Running', amount: 1 },
-    { name: 'Hiking', amount: 1 },
-    { name: 'Swimming', amount: 1 },
-    { name: 'Yoga', amount: 1 },
-    { name: 'Camping-Outdoors', amount: 1 },
-    { name: 'Camping-Indoors', amount: 1 },
-    { name: 'Camping-Underwater', amount: 1 },
-  ];
-
-  private calculateBadgeProgress() {
-  }
-
-  get badges() {
-    return this._badges;
-  }
-
-  get user() {
-    return this._user;
-  }
-
-  get testactivities() {
-    return this._testactivities;
-  }
-
-  public goBack(): void {
-    this._location.back();
-  }
 
   /**
-   * @param _location Provides the 
-   * @param _us Provides the navigation and url manipulation capabilities.
-   * @param _ps Provides the profile interaction with the fire batadase?...
-   * @param _route Provides the route services for 
+   * @param _location Location is a service that applications can use to interact with a browser's URL.
+   * @param _us Service that provides current user object.
+   * @param _ps Service that provides profile of a user as an object.
+   * @param _route Contains the information about a route associated with a component loaded in an outlet.
    */
   constructor(private _location: Location, private _us: UserService,
-              private _ps: ProfilesService, private _route: ActivatedRoute) { }
+    private _ps: ProfilesService, private _route: ActivatedRoute) { }
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   * Subscribes to route to see if id exists, if not gets current user profile, otherwise gets profile of id.
+   */
   ngOnInit() {
     this._idSubscription = this._route.params.subscribe(params => {
       const id = params['id'];
@@ -78,4 +46,49 @@ export class ProfileComponent implements OnInit {
     });
   }
 
+  /**
+   * Lifecycle hook that is called when a directive, pipe or service is destroyed.
+   * Kills subscription to route params.
+   */
+  ngOnDestroy() {
+    this._idSubscription.unsubscribe();
+  }
+
+  /**
+   * Get function for user's profile observable.
+   * @returns Observable of user's profile.
+   */
+  get user(): Observable<any> {
+    return this._user;
+  }
+
+  /**
+   * Determines color of chip based on index.
+   * @param index Index of chip.
+   * @returns Either primary or accent color string.
+   */
+  public chipColor(index: number): string {
+    if (index === 0) {
+      return 'primary';
+    } else if (index === 1) {
+      return 'accent';
+    }
+  }
+
+  /**
+   * Navigates back in the platform's history.
+   */
+  public goBack(): void {
+    this._location.back();
+  }
+
+  /**
+   * Sorts activities array of profile by count.
+   * @param activities Activities of user's profile.
+   * @returns Sorted array.
+   */
+  public rankActivities(activities: any[]): any[] {
+    if (!Array.isArray(activities)) { return; }
+    return activities.sort((a: any, b: any) => b.count - a.count);
+  }
 }
