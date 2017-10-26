580d19d5235436191396ee07187c40a0725e95ea
Michael Solati <mkslt04@gmail.com>
2017-10-26 16:22:17 -0400

    fix(swipe): fix swipe of cards on place page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import * as Hammer from 'hammerjs';
 
 import { environment } from '../environments/environment';
 
@@ -10,6 +11,11 @@ import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export class HammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    'swipe': { direction: Hammer.DIRECTION_ALL }
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,6 +28,10 @@ import { AppComponent } from './app.component';
       apiKey: environment.google
     })
   ],
+  providers: [{
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: HammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
